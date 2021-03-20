5ada49b64246b40cb65041df5233bba9221a0f5f
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2021-03-20 14:03:20 +0700

    BTVN

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	do {
+		cout << "Nhap n = ";
+		cin >> n;
+		if (n <= 0) cout << "Nhap lai...";
+	} while (n <= 0);
+	int dong = n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 1; j < dong; j++)
+		{
+			cout << " ";
+		}
+		for (int k = 2 * (dong - 1); k < 2 * n - 1; k++)
+		{
+			cout << "*";
+		}
+		dong--;
+		cout << endl;
+	}
+
+}
