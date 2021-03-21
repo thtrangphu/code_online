aa6f7bc50142884d5e2c5ece741f8ebdd7d6f253
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2021-03-22 06:08:17 +0700

    Bai2/21/3/2021
    Xep hang hoc luc

---
+++ bai2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void inputMark(float &x)
+{
+	do{
+		cout << "Moi ban nhap diem: ";
+		cin >> x;
+	} while (x < 0 || x>10);
+	
+}
+void checkMark(float x)
+{
+	if (x >= 9 && x <= 10) cout << "Gioi";
+	else if (x >= 8) cout << "Kha";
+	else if (x >= 4) cout << "Trung binh";
+	else cout << "Yeu";
+}
+int main()
+{
+	float x = 0;
+	inputMark(x);
+	checkMark(x);
+}
