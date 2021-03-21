e1e637ecac9fe666dac8606e2f660668bb5f8962
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2021-03-22 06:15:15 +0700

    Bai 4
    Tinh tuoi

---
+++ bai4.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int birthYear(int nowyear, int age)
+{
+	cout << "Nhap nam hien tai: ";
+	cin >> nowyear;
+	cout << "Nhap tuoi: ";
+	cin >> age;
+	return nowyear - age;
+}
+int main()
+{
+	int a = 4, b = 5;
+	cout << "Ban sinh nam: " << birthYear(a, b);
+
+}
