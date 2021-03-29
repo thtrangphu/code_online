f3f69fbf2678feb03892e8955d6fcd67d4a17595
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2021-03-29 18:39:19 +0700

    Bai1 - Gop mang

---
+++ Bai1.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+using namespace std;
+void NhapMang(int a[], int& n)
+{
+	cout << "Nhap so luong phan tu muon thuc hien: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << " \t\t\t";
+		cout << "a[" << i << "]=";
+		cin >> a[i];
+	}
+}
+//
+void XuatMang(int a[], int n)
+{
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]=" << a[i] << "	  ";
+	}
+}
+void GopMang(int a[], int b[], int n1, int &n2)
+{
+	int c[40];
+	for (int i = 0; i < n1; i++)
+	{
+		c[i] = a[i];
+	}
+	int dem = n1;
+	for (int i = 0; i <  n2; i++)
+	{
+		c[dem] = b[i];
+		dem++;
+	}
+	n2 = n1 + n2;
+	dem = 0;
+	for (int i = 0; i <  n2; i++)
+	{
+		b[i] = c[dem];
+		dem++;
+	}
+}
+int main() {
+	int a[20], b[20];
+	int n1, n2;
+	cout << "Nhap mang 1: " << endl;
+	NhapMang(a, n1);
+	cout << "Nhap mang 2: " << endl;
+	NhapMang(b, n2);
+	cout << "\nXUAT MANG 1: \n";
+	XuatMang(a, n1);
+	cout << "\nXUAT MANG 2: \n";
+	XuatMang(b, n2);
+	GopMang(a, b, n1, n2);
+	cout << "\nMang sau khi gop la: " << endl;
+	XuatMang(b, n2);
+	return 0;
+}
